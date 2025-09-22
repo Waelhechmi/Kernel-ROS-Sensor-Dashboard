d0a361996ecd7dc6c6394517110387fc0c1ecf11
Hechmi_wael <waelhechmi100@gmail.com>
2025-09-22 13:10:02 +0100

    Supprimer le fichier sensor_publisher.cpp

--- ros2_ws/build/ros_publisher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wael/Kernel-ROS/Kernel-ROS-Sensor-Dashboard/ros2_ws/src/ros_publisher
-# Build directory: /home/wael/Kernel-ROS/Kernel-ROS-Sensor-Dashboard/ros2_ws/build/ros_publisher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
