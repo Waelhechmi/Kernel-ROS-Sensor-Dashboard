52ac328094ca7d166af7a63c0d58747e1085f25a
Hechmi_wael <waelhechmi100@gmail.com>
2025-09-23 12:04:36 +0100

    update ReadME file

--- sensors_publisher/build/sensors_publisher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wael/Kernel-ROS/Kernel-ROS-Sensor-Dashboard/sensors_publisher
-# Build directory: /home/wael/Kernel-ROS/Kernel-ROS-Sensor-Dashboard/sensors_publisher/build/sensors_publisher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
